1b9c790e7ab3b0023755c1105e8939912d38c9fb
henrik392 <henrik.halvorsen.kvamme@gmail.com>
2020-03-22 20:28:59 +0100

    Fixed critical error + new features
    Fixed critical error and added support for custom enviroment variables in the "setting.json" file. Also added comments.

--- auto_sort.py
+++ auto_sort.py
@@ -21,42 +21,53 @@ def check_folder(src, extension):
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
+        # Track each specified directories
         for i in range(len(settings_data["analyze"])):
-            folder_to_track = homedir + settings_data["analyze"][i]["track"]
+            # Checks each file in folder_to_track
+            folder_to_track = settings_data["analyze"][i]["track"]
             for file in os.listdir(folder_to_track):
-                src = folder_to_track + "/" + file
-                _, file_extension = os.path.splitext(src)
+                # Not hidden file
+                if file[0] != ".":
+                    src = folder_to_track + "/" + file
+                    _, file_extension = os.path.splitext(src)
 
-                # Sort to correct location
-                folder_dests = settings_data["analyze"][i]["dest"]
-                for j in range(len(folder_dests)):
-                    current_extensions = settings_data["ext"][folder_dests[j]["ext"]].lower()
-                    current_destination = folder_dests[j]["dest"]
-                    correct_ext = False
-                    
-                    if os.path.isdir(src):
-                        correct_ext = check_folder(src, current_extensions)
-                    elif file_extension.lower() in current_extensions.lower():
-                        correct_ext = True
-                    
-                    # Assign folder destination
-                    new_name = file
+                    # Sort to correct location
+                    folder_dests = settings_data["analyze"][i]["dest"]
+                    for j in range(len(folder_dests)):
+                        current_extensions = settings_data["ext"][folder_dests[j]["ext"]].lower()
+                        current_destination = folder_dests[j]["dest"]
+                        correct_ext = False
+                        
+                        # Check if file match with current parameter, if it is a directory, chekcs if 1/3 of files in folder match with current parameters
+                        if os.path.isdir(src):
+                            correct_ext = check_folder(src, current_extensions)
+                        elif file_extension.lower() in current_extensions.lower():
+                            correct_ext = True
+                        
+                        new_name = file
 
-                    if correct_ext:
-                        folder_destination = current_destination.replace("HOMEDIR", homedir)
-                        new_destination = folder_destination + "/" + new_name
-                        os.rename(src, new_destination)
+                        # Assign folder destination
+                        if correct_ext:
+                            folder_destination = current_destination
+                            new_destination = folder_destination + "/" + new_name
+                            os.rename(src, new_destination)
 
 homedir = os.environ['HOME']
-with open('settings.json') as json_file:
-    settings_data = json.load(json_file)
+with open('settings.json', 'r+') as file:
+    content = file.read()
+    file.seek(0)
+    content_split = content.split("$")
+    for i in range(1, len(content_split)-1, 2):
+        content = content.replace('$' + content_split[i] + '$', os.environ[content_split[i]])
+    settings_data = json.loads(content)
 
 event_handler = MyHandler()
 observer = Observer()
 
-paths = [d["track"].replace("HOMEDIR", homedir) for d in settings_data["analyze"]]
+paths = [d["track"] for d in settings_data["analyze"]]
+
 for i in paths:
-    targetPath = i
+    targetPath = str(i)
     observer.schedule(event_handler, targetPath, recursive=True)
     observer.start()
 
